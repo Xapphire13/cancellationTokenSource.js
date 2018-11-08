b8eefe99d4a0c3af9350fc4af521048552693466
Steven Hobson-Campbell <xapphire13@live.com>
2018-11-07 18:09:32 -0800

    fix: Re-export correctly in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import _CancellationToken from "./cancellation-token";
-import _CancellationTokenSource from "./cancellation-token-source";
-import _OperationCanceledError from "./operation-canceled-error";
-
-export const CancellationToken = _CancellationToken;
-export const CancellationTokenSource = _CancellationTokenSource;
-export const OperationCanceledError = _OperationCanceledError;
+export { default as CancellationToken } from "./cancellation-token";
+export { default as CancellationTokenSource } from "./cancellation-token-source";
+export { default as OperationCanceledError } from "./operation-canceled-error";
